5c7c4ceca12bdaf5c170ccbde236123248422881
SheltonDomingos <125916779+SheltonDomingos@users.noreply.github.com>
2023-03-25 12:02:53 -0300

    Add files via upload

---
+++ exercicio_pergunta_1.py
@@ -0,0 +1,9 @@
+k = 0
+indice = 13
+soma = 0
+class int(
+    indice = 13, soma = 0, k = 0):
+        while k < indice:
+            k = k + 1
+            soma = soma + k
+            print(soma)    
